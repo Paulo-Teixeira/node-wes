71e72d259591e0dc4235ee7d184340ac8fd196e3
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-04 15:35:26 +0200

    Adds register method to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
   res.render('login', { title: 'Login' });
-}
+};
 
 exports.registerForm = (req, res) => {
   res.render('register', { title: 'Register' });
-}
+};
 
 exports.validateRegister = (req, res, next) => {
   req.sanitizeBody('name');
@@ -28,4 +30,11 @@ exports.validateRegister = (req, res, next) => {
     return; // Stop the fn from running
   }
   next(); // There where no errors
-}
+};
+
+exports.register = async (req, res, next) => {
+  const user = new User( { email: req.body.email, name: req.body.name });
+  const register = promisify(User.register, User);
+  await register(user, req.body.password);
+  next(); // Pass to authController.login
+};
