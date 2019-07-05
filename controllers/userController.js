42c11fd431beb90446103005d9fe7c69e9277f5b
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-05 16:36:44 +0200

    Fixes update user crash

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,7 +55,6 @@ exports.updateAccount = async (req, res) => {
     { new: true, runValidators: true, context: 'query' }
   );
 
-  res.json(user);
   req.flash('success', 'Updated the profile!');
-  //res.redirect('back');
+  res.redirect('back');
 };
--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ router.post('/register',
 router.get('/logout', authController.logout);
 
 router.get('/account', authController.isLoggedIn, userController.account);
-router.post('/account', catchErrors(userController.account));
+router.post('/account', catchErrors(userController.updateAccount));
 router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token',
