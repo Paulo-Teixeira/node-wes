330a6d6bb0a985b386216bcf73c25218fe4f73aa
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-10 11:41:56 +0200

    Adds review support and advanced relationship population.

--- models/Store.js
+++ models/Store.js
@@ -38,6 +38,9 @@ const storeSchema = new mongoose.Schema({
     ref: 'User',
     required: 'You must supply an author'
   }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 
 // Define indexes
@@ -69,5 +72,12 @@ storeSchema.statics.getTagsList = function() {
     { $sort: { count: -1 } }
   ]);
 }
+
+// Find reviews where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+  ref: 'Review', // What model to link?
+  localField: '_id', // Which field on the store?
+  foreignField: 'store' // Which field on the review?
+});
 
 module.exports = mongoose.model('Store', storeSchema);
